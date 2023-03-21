5b4cbf3bb08c2aeab7ec6b4ec35d7cb1f2c5360f
Shaggy <shaggy@Shaggys-MacBook-Pro.local>
2023-03-21 13:32:21 +0500

    recoever

--- routes/foundPerson.js
+++ routes/foundPerson.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllPersons,createPerson,getPerson,updatePerson,deletePerson} = require('../controllers/foundPerson');
+const {getAllPersons,createPerson,getPerson,updatePerson,deletePerson,recoverPerson} = require('../controllers/foundPerson');
 
 router.get('/', getAllPersons); //get all tasks
 router.get('/:id', 	getPerson); // get single task
 router.patch('/:id', updatePerson); // update single task
 router.delete('/:id', deletePerson); // delete single task
 router.post('/', createPerson); // create single task
+router.put('/:id', recoverPerson); // create single task
 
 module.exports = router;
